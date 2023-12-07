020a9fa0fa7e7114537a6143bd83fd9b8671a838
Mariale <male2593@gmail.com>
2023-12-07 08:23:39 -0500

    adding pedac for exercise 8

---
+++ Exercises/Easy_1/leapYears1.js
@@ -0,0 +1,14 @@
+/* 
+Problem: 
+
+-write a function that takes 1 argument (number)
+- determine is the number in the argument is evenly 
+divisible by 4, 100 and 400.
+-input: number
+-output: boolean.
+- the number take by the argument needs to be 
+a positive integer.
+
+
+
+*/
