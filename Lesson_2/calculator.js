2eecb30cd798a579e2d3827e5250f6f1cc328c85
Mariale <male2593@gmail.com>
2023-12-26 22:58:43 -0500

    first version

--- Lesson_2/calculator.js
+++ Lesson_2/calculator.js
@@ -8,6 +8,7 @@ function invalidNumber(number) {
   return number.trimStart() === '' || Number.isNaN(Number(number));
 }
 
+
 prompt('Welcome to Calculator!');
 
 prompt('What\'s the first number?');
@@ -34,24 +35,65 @@ while (!['1', '2', '3', '4'].includes(operation)) {
   operation = readLine.question();
 }
 
-let output;
+function calculator (number1, number2, operator) {
 
-switch (operation) {
-  case '1':
-    output = Number(firstNumber) + Number(secondNumber);
-    break;
-  case '2':
-    output = Number(firstNumber) - Number(secondNumber);
-    break;
-  case '3':
-    output = Number(firstNumber) * Number(secondNumber);
-    break;
-  case '4':
-    output = Number(firstNumber) / Number(secondNumber);
-    break;
+  let output;
+
+  switch (operator) {
+    case '1':
+      output = Number(number1) + Number(number2);
+      break;
+    case '2':
+      output = Number(number1) - Number(number2);
+      break;
+    case '3':
+      output = Number(number1) * Number(number2);
+      break;
+    case '4':
+      output = Number(number1) / Number(number2);
+      break;
+  }
+
+  prompt(`The result is : ${output}`);
 }
 
+calculator(firstNumber, secondNumber, operation);
 
-prompt(`The result is : ${output}`);
+prompt('Would you like to make another calculation?\nyes no\n');
+let again = readLine.question();
 
+while (again !== 'no' && again !== 'yes') {
+  prompt('Must choose yes or no');
+  again = readLine.question();
+}
+
+while (again === 'yes') {
+  prompt('What\'s the first number?');
+  firstNumber = readLine.question();
+
+  while (invalidNumber(firstNumber)) {
+    prompt('Hmm... that doesn\'t look like a valid number.');
+    firstNumber = readLine.question();
+  }
+
+  prompt('What\'s the second number?');
+  secondNumber = readLine.question();
+
+  while (invalidNumber(secondNumber)) {
+    prompt('Hmm... that doesn\'t look like a valid number.');
+    secondNumber = readLine.question();
+  }
+
+  prompt(' What operation would you like to perform?\n 1) add 2) Subtract 3) Multiply 4) Divide');
+  operation = readLine.question();
+
+  while (!['1', '2', '3', '4'].includes(operation)) {
+    prompt('Must choose 1, 2, 3 or 4');
+    operation = readLine.question();
+  }
+
+  calculator(firstNumber, secondNumber, operation);
+  prompt('Would you like to make another calculation?\nyes no\n');
+  again = readLine.question();
+}
 
